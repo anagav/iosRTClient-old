ad3a06d1e64f9d8363e654d2899c52757fe0b540
ashishn <ashish.nagav@yahoo.com>
2014-06-04 20:51:06 -0700

    Initial Commit

---
+++ RottenTomatoesClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoesClient
+//
+//  Created by Ashish Nagavaram on 6/4/14.
+//  Copyright (c) 2014 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
